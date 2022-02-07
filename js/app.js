728fc68c4cc9b59f6b96f561d854eaeca6206346
stefanom13 <stefanomancuso13@gmail.com>
2022-02-07 19:33:05 +0100

    inserimento numeri js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('ciao a tutti')
+
+// collegare variabile  contenitore in html
+const contenitore = document.getElementById('first-container');
+
+
+// inizializzare un programma che stampi i numeri da 1 a 100
+for ( let i = 1; i < 101; i++){
+    console.log(i)
+}
+// selezionare numeri multipli di 3 e 5 e modificare aspetto 
+
+// selezionare solo numeri multipli di 3  e modificare aspetto 
+
+// selezionare solo numeri multipli di 5  e modificare aspetto 
+
+
+
