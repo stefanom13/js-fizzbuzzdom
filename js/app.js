604b01cfe9f13392b0907405a5784e1b9931a572
stefanom13 <stefanomancuso13@gmail.com>
2022-02-08 14:52:48 +0100

    esercizio completo , si può migliorare.

--- js/app.js
+++ js/app.js
@@ -6,12 +6,27 @@ const contenitore = document.getElementById('first-container');
 // inizializzare un programma che stampi i numeri da 1 a 100
 for ( let i = 1; i < 101; i++){
     console.log(i)
+
+    const squareElement = document.createElement('div');
+    squareElement.classList.add('box');
+    contenitore.append(squareElement);
+
+    // selezionare numeri multipli di 3 e 5 e modificare aspetto 
+    if ( i % 15 === 0){
+        squareElement.classList.add('box--r');
+        squareElement.append('fizzbuzz');
+     
+    } else if( i % 5 === 0){
+        squareElement.classList.add('box--y');
+        squareElement.append('buzz');
+
+    } else if( i % 3 === 0){
+        squareElement.classList.add('box--g');
+        squareElement.append('fizz');
+    } else 
+     squareElement.append(i);
 }
-// selezionare numeri multipli di 3 e 5 e modificare aspetto 
-
-// selezionare solo numeri multipli di 3  e modificare aspetto 
-
-// selezionare solo numeri multipli di 5  e modificare aspetto 
+ 
 
 
 
